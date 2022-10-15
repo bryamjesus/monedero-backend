c6db84578f2163574baf79a6430a0ba30997ad3f
Bryam Jesus Talledo Garcia <btalledog@odybank.com.pe>
2022-10-14 20:59:14 -0500

    Finish API

--- src/controllers/registroController.js
+++ src/controllers/registroController.js
@@ -1,7 +1,7 @@
 const model = require('../models/registroModel')
 
 const controlador = {
-  async listar(req, res) {
+  async listAllRegister(req, res) {
     try {
       const resp = await model.find()
       res.json(resp)
@@ -9,7 +9,60 @@ const controlador = {
       console.log(error)
       res.sendStatus(500)
     }
-  }
+  },
+  async listOneRegister(req, res) {
+    const { id } = req.params
+    try {
+      const result = await model.findById(id);
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  async createOneRegister(req, res) {
+    const { description, typeRegister, amount } = req.body
+    const dato = new model;
+    dato.description = description;
+    dato.typeRegister = typeRegister;
+    dato.amount = amount;
+    try {
+      const result = await dato.save();
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  async updateOneRegister(req, res) {
+    const { id } = req.params
+    const { description, typeRegister, amount } = req.body
+    const datos = {
+      description, typeRegister, amount
+    }
+
+    try {
+      const result = await model.findByIdAndUpdate(id, datos, { new: true })
+      res.json(result)
+    }
+    catch (error) {
+      console.log(error)
+      res.sendStatus(500)
+    }
+
+
+  },
+  async deleteOneRegister(req, res) {
+    const { id } = req.params
+    try {
+      await model.findByIdAndDelete(id);
+      res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+
 }
 
 module.exports = controlador
--- src/routes/registroRoute.js
+++ src/routes/registroRoute.js
@@ -3,7 +3,24 @@ const router = express.Router()
 const controlador = require('../controllers/registroController')
 
 router.get('/', (req, res) => {
-  controlador.listar(req,res)
+  controlador.listAllRegister(req, res)
 })
+
+router.get('/:id', (req, res) => {
+  controlador.listOneRegister(req, res)
+})
+
+router.post('/', (req, res) => {
+  controlador.createOneRegister(req, res)
+})
+
+router.put('/:id', (req, res) => {
+  controlador.updateOneRegister(req, res)
+})
+
+router.delete('/:id', (req, res) => {
+  controlador.deleteOneRegister(req, res)
+})
+
 
 module.exports = router
